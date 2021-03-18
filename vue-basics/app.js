cf250b5d2967a296048612ab05c7335b2c398897
Brandon Swatek <Brandon.Swatek@pl.ey.com>
2021-03-18 13:05:01 +0100

    basics done

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -1,8 +1,20 @@
 Vue.createApp({
     data() {
         return {
-            goals = [],
-            newGoal = ''
+            courseGoal: 'Be competent intern :)',
+            courseGoal2: '',
+            vueLink: 'https://vuejs.org/'
         }
-    }
+    },
+    methods: {
+        outputGoal() {
+            const randomNum = Math.random();
+            if (randomNum < 0.5) {
+                this.courseGoal2 = 'Learn Vue';
+            } else {
+                this.courseGoal2 = 'Master Vue';
+            }
+            return '<h3>'.concat(this.courseGoal2.concat('<h3>'));
+        }
+    },
 }).mount('#user-goal');
