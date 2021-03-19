fcf579ced3e37a1881635ab9e6ec05706a5e4fba
Brandon Swatek <Brandon.Swatek@pl.ey.com>
2021-03-19 12:59:19 +0100

    conditionals done

--- vue-conditionals/app.js
+++ vue-conditionals/app.js
@@ -1,6 +1,22 @@
+// v-show alternatywa dla v-if, ale element zawsze jest
+// wysyłany do html, a widoczność zależy od css display
+
+// v-for inna składnia: v-for="(item, index) in goals"
+// v-for="value in {name: 'Max', age: 32}" można iterować po obiektach (wartości/klucze)
+// v-for="(value, key, index) in  {name: 'Max', age: 32}"
+// v-for="num in 10"
+
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { goals: [], inputGoal: ''};
+  },
+  methods: {
+    addGoal() {
+      this.goals.push({title: this.inputGoal, note: ''});
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
+    }
   },
 });
 
