59b7bd5f206c8c88aea0a2d2998c2d908b09e70e
Brandon Swatek <Brandon.Swatek@pl.ey.com>
2021-03-21 18:09:03 +0100

    did behind the scenes module

---
+++ vue-behind-the-scenes/app.js
@@ -0,0 +1,59 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.$refs.userText.value;
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  }
+});
+
+app.mount('#app');
+
+setTimeout(() => {
+  app.unmount();
+}, 10000);
+
+const app2 = Vue.createApp({
+  template: `
+  <p> {{favouriteMeal}} </p>
+  `,
+  data() {
+    return {
+      favouriteMeal: 'Blueberry Chorizo'
+    }
+  },
+});
+
+app2.mount('#app2');
