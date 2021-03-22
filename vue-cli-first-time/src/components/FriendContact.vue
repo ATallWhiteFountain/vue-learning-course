1ff89e4c758e85e896c8ea5cb49d50b12b4108fc
Brandon Swatek <Brandon.Swatek@pl.ey.com>
2021-03-22 21:09:49 +0100

    components intro

---
+++ vue-cli-first-time/src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ buttonMsg }}</button>
+        <ul v-if="detailsVisible">
+            <li><strong>Phone: </strong>{{ friend.phone }}</li>
+            <li><strong>Email: </strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'Friend',
+    data() {
+        return {
+            detailsVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '012 345 678',
+                email: 'manuel.lorenz@lchst.com'
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsVisible = !this.detailsVisible;
+        }
+    },
+    computed: {
+        buttonMsg() {
+            return this.detailsVisible ? 'Hide details' : 'Show details';
+        }
+    }
+}
+</script>
